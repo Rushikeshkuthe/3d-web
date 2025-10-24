b292ddc14b3f80f829a3681d192fcb0feb02586b
Rushikeshkuthe <126712461+Rushikeshkuthe@users.noreply.github.com>
2025-10-24 18:41:01 +0530

    added 3d modal

---
+++ mac-gsap-app/src/components/ProductViewer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import useMacBookStore from '../store'
+import clsx from 'clsx'
+import {Canvas} from '@react-three/fiber'
+import {Box, OrbitControls} from '@react-three/drei'
+import MacBookModel14 from './models/Macbook-14'
+import StudioLights from './three/StudioLights'
+import ModalSwitcher from './three/ModalSwitcher'
+import { useMediaQuery } from 'react-responsive'
+
+const ProductViewer = () => {
+    const {color, scale , setColor , setScale} = useMacBookStore()
+    const isMobile =useMediaQuery({query:'(max-width: 1024px)'});
+  return (
+    <section id='product-viewer'>
+        <h2>Take a closer look. </h2>
+      <div className='controls'>
+        {/* <p className='info'>MacBook Pro | Available in 14" & 16" in Space Gray and Dark</p> */}
+        <div className='flex-center gap-5 mt-5'>
+            <div className='color-control'>
+                <div 
+                className={clsx('bg-neutral-300',color=== '#adb5bd'&& 'active')}
+                onClick={()=>setColor('#adb5bd')}
+                />
+               <div 
+                className={clsx('bg-neutral-900',color=== '#2e2c2e'&& 'active')}
+                onClick={()=>setColor('#2e2c2e')}
+                />
+            </div>
+            <div className='size-control'>
+                <div
+                 className={clsx(scale===0.06 ? 'bg-white text-black':'bg-transparent text-white')}
+                onClick={()=>setScale(0.06)}
+                >
+                    <p>14"</p>
+                    </div>
+                <div
+                 className={clsx(scale===0.08 ? 'bg-white text-black':'bg-transparent text-white')}
+                onClick={()=>setScale(0.08)}
+                ><p>16"</p></div>
+
+            </div>
+        </div>
+      </div>
+      <Canvas id='canvas' camera={{position:[0,2,5],fov:50,near:0.1,far:100}}>
+        <StudioLights/>
+      
+        <ModalSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}/>
+      </Canvas>
+    </section>
+  )
+}
+
+export default ProductViewer
